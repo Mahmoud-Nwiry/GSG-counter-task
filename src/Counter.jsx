995de8e0849a8c68a9c23b1dec83be5f7ff40f16
Mahmoud-Nwiry <mamo992001@gmail.com>
2022-12-20 11:36:04 +0200

    organize the code by prettier

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,34 +1,45 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Counter extends Component {
+  state = {
+    count: this.props.initCount || 0,
+  };
 
-    state = {
-        count : this.props.initCount || 0
-    }
+  componentDidMount() {
+    this.props.getTotal(this.state.count);
+  }
 
-    componentDidMount(){
-      this.props.getTotal(this.state.count);
-    }
+  increment = () => {
+    this.setState((prevState) => ({
+      count: this.props.initCount
+        ? prevState.count + this.props.initCount
+        : prevState.count + 1,
+    }));
+    this.props.getTotal(this.props.initCount || 1);
+  };
 
-    increment = () => {
-        this.setState(prevState => ({count : this.props.initCount ? prevState.count + this.props.initCount : prevState.count + 1}));
-        this.props.getTotal(this.props.initCount || 1)
+  deccrement = () => {
+    if (this.state.count > 0) {
+      this.setState((prevState) => ({
+        count: this.props.initCount
+          ? prevState.count - this.props.initCount
+          : prevState.count - 1,
+      }));
+      this.props.getTotal(-this.props.initCount || -1);
     }
-
-    deccrement = () => {
-        if(this.state.count > 0){
-            this.setState(prevState => ({count : this.props.initCount ? prevState.count - this.props.initCount : prevState.count - 1}))   
-            this.props.getTotal(-this.props.initCount || -1)
-        }
-    }
+  };
 
   render() {
     return (
-      <div className='counter_box'>
+      <div className="counter_box">
         <p>{this.state.count}</p>
-        <button className='btn deccress' onClick={this.deccrement}>-</button>
-        <button className='btn inccress' onClick={this.increment}>+</button>
+        <button className="btn deccress" onClick={this.deccrement}>
+          -
+        </button>
+        <button className="btn inccress" onClick={this.increment}>
+          +
+        </button>
       </div>
-    )
+    );
   }
 }
